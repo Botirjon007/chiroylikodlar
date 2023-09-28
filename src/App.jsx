26883ec0bae60607267c83ab32aa62307f1bfd84
Botirjon007 <batyrzhanulmasov@gmail.com>
2023-09-28 23:34:01 +0500

    fayllar qoshildi

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import NavbarContainer from "./components/Navbar";
+import Section from "./components/Section";
+const App = () => {
+  return (
+    <div className="App">
+      {/* <NavbarContainer />         */}
+      <Section fullName = "Jamshidbek Kobilov" age={10}/>        
+      <Section fullName = "Azizbek Khojayev" age={99}/>        
+      <Section fullName = "Jamshidbek Kobilov" age={10}/>        
+      <Section fullName = "Azizbek Khojayev" age={99}/>        
+      <Section fullName = "Jamshidbek Kobilov" age={10}/>        
+      <Section fullName = "Azizbek Khojayev" age={99}/>        
+      <Section fullName = "Jamshidbek Kobilov" age={10}/>        
+      <Section fullName = "Azizbek Khojayev" age={99}/>        
+      <Section fullName = "Jamshidbek Kobilov" age={10}/>        
+      <Section fullName = "Azizbek Khojayev" age={99}/>        
+      <Section fullName = "Jamshidbek Kobilov" age={10}/>        
+      <Section fullName = "Azizbek Khojayev" age={99}/>        
+    </div>
+  );
+}
+
+export default App;
